54002d351794d863762b5c411b7d4eda8b789038
Suyeon Yang <yan17002@byui.edu>
2020-09-17 13:45:14 -0600

    Added output

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,15 +6,18 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            string name;
-            name = "Suyeon Yang";
-            String location = "Rexburg, Idaho";
-            Console.WriteLine("My name is " + name + ". I am from " + location);
+            string name = "Suyeon Yang";
+            string location = "Rexburg, Idaho";
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I am from " + location);
 
             Console.WriteLine(DateTime.Now.ToString("d"));
 
             DateTime christmas = new DateTime(2020, 12, 25);
             Console.WriteLine((christmas - DateTime.Now).Days + " days");
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
