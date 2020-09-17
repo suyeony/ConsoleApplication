57c45a7d1acf08de6978494650d772a0a5735dbd
Suyeon Yang <yan17002@byui.edu>
2020-09-17 13:18:37 -0600

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = "Suyeon Yang";
+            String location = "Rexburg, Idaho";
+            Console.WriteLine("My name is " + name + ". I am from " + location);
+
+            Console.WriteLine(DateTime.Now.ToString("d"));
+
+            DateTime christmas = new DateTime(2020, 12, 25);
+            Console.WriteLine((christmas - DateTime.Now).Days + " days");
+        }
+    }
+}
