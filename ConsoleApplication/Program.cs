b268d269979a4af3e789b831dfa67554961d4570
Suyeon Yang <yan17002@byui.edu>
2020-09-19 13:20:17 -0600

    Finish week1 project

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,15 +6,49 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            string name = "Suyeon Yang";
-            string location = "Rexburg, Idaho";
-            Console.WriteLine("My name is " + name);
-            Console.WriteLine("I am from " + location);
+            // 2.
+            string name;
+            Console.Write("Please enter your full name: ");
+            name = Console.ReadLine();
 
-            Console.WriteLine(DateTime.Now.ToString("d"));
+            string location;
+            Console.Write("Please enter your location: ");
+            location = Console.ReadLine();
 
+             // 3.
+            Console.WriteLine($"Your name is {name}");
+            Console.WriteLine($"You are from {location}");
+
+            // 4.
+            var currentDate = DateTime.Now;
+            Console.WriteLine("The current date is " + currentDate.ToShortDateString());
+
+            // 5.
             DateTime christmas = new DateTime(2020, 12, 25);
             Console.WriteLine((christmas - DateTime.Now).Days + " days");
+
+
+            // 6.
+            double width, height, woodLength, glassArea; 
+            string widthString, heightString;
+
+            Console.Write("Please enter width: (in meters) ");
+            widthString = Console.ReadLine(); 
+            width = double.Parse(widthString);
+
+            Console.Write("Please enter height: (in meters) ");
+            heightString = Console.ReadLine(); 
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet"); 
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+
+
+            // 7.
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
